4b96a1676a309ecb6f0ad0a3b1e7b84fabce8665
mmansion <mikhail@transmission.studio>
2021-03-10 08:18:03 -0500

    catching up on committing sketches

--- sketches/src/n-0048.rs
+++ sketches/src/n-0048.rs
@@ -1,252 +1,207 @@
-//! A demonstration of playing back a sequence of images.
-//!
-//! This approach loads a directory of images into a single texture array. We only ever present a
-//! single layer of the texture at a time by creating a texture view. We select which layer to view
-//! by using a `current_layer` variable and updating it based on a frame rate that we determine by
-//! the mouse x position.
-//!
-//! An interesting exercise might be to make a copy of this example and attempt to smooth the slow
-//! frame rates by interpolating between two of the layers at a time. Hint: this would likely
-//! require adding a second texture view binding to the bind group and its layout.
-
-use nannou::image;
-use nannou::image::RgbaImage;
 use nannou::prelude::*;
-use std::path::{Path, PathBuf};
+use nannou::noise::*;
+use nannou::Draw;
+use std::collections::VecDeque;
+use std::time::Duration;
 
-struct Model {
-    current_layer: f32,
-    texture_array: wgpu::Texture,
-    texture_view: wgpu::TextureView,
-    sampler: wgpu::Sampler,
-    bind_group_layout: wgpu::BindGroupLayout,
-    bind_group: wgpu::BindGroup,
-    render_pipeline: wgpu::RenderPipeline,
-    vertex_buffer: wgpu::Buffer,
-}
+pub mod lib;
 
-// The vertex type that we will use to represent a point on our triangle.
-#[repr(C)]
-#[derive(Clone, Copy)]
-struct Vertex {
-    position: [f32; 2],
-}
+// module tree
+use crate::lib::points::Point as Point;
+use crate::lib::vehicles::Vehicle as Vehicle;
 
-// The vertices that make up the rectangle to which the image will be drawn.
-const VERTICES: [Vertex; 4] = [
-    Vertex {
-        position: [-1.0, 1.0],
-    },
-    Vertex {
-        position: [-1.0, -1.0],
-    },
-    Vertex {
-        position: [1.0, 1.0],
-    },
-    Vertex {
-        position: [1.0, -1.0],
-    },
-];
+static CAPTURE  : bool = true; // capture to image sequence
+static WIDTH    : i32 = 800;
+static HEIGHT   : i32 = 800; 
+static ANGLES   : i32 = 3;
 
 fn main() {
     nannou::app(model).update(update).run();
 }
 
+struct Model {
+    points : Vec<Vector2>,
+    incs   : Vec<f32>,
+    noise  : Perlin,
+    xOff   : f64, 
+    yOff   : f64,
+    this_capture_frame : i32,
+    last_capture_frame : i32,
+    new_frame : bool,
+    last_calc : Duration,
+    inc : f32,
+}
+
+// ----------------------------------------------------------------------
 fn model(app: &App) -> Model {
-    // Load the images.
-    let sequence_path = app
-        .assets_path()
-        .unwrap()
-        .join("images")
-        .join("spinning_dancer");
 
-    println!("Loading images...");
-    let (images, (img_w, img_h)) = load_images(&sequence_path);
-    println!("Done!");
-
-    let w_id = app
-        .new_window()
-        .size(img_w, img_h)
+    let rect = Rect::from_w_h( WIDTH, HEIGHT );
+    
+    // app.set_loop_mode(LoopMode::loop_once());
+    app.set_loop_mode(LoopMode::rate_fps(0.1));
+    
+    app.new_window()
+        .size(800, 800)
         .view(view)
         .build()
         .unwrap();
 
-    let window = app.window(w_id).unwrap();
-    let device = window.swap_chain_device();
-    let format = Frame::TEXTURE_FORMAT;
-    let msaa_samples = window.msaa_samples();
+    let mut noise = Perlin::new();
+    noise = noise.set_seed(1);
+    let mut xOff = 0.0;
+    let mut yOff = 0.0;
+    let mut points = Vec::new();
+    let mut incs = Vec::new();
+    let mut new_frame = false;
+    let mut last_calc = Duration::from_millis(0);
+    let mut inc = 0.0;
+    //----------------------------------
+    let mut this_capture_frame = 0;
+    let mut last_capture_frame = 0;
+    //----------------------------------
 
-    let vs_mod = wgpu::shader_from_spirv_bytes(device, include_bytes!("shaders/vert.spv"));
-    let fs_mod = wgpu::shader_from_spirv_bytes(device, include_bytes!("shaders/frag.spv"));
+    // setup incs
+    for i in 0..ANGLES+1 {
+        incs.push(i as f32 * random_f32());
+    }
 
-    let texture_array = {
-        // The wgpu device queue used to load the image data.
-        let queue = window.swap_chain_queue();
-        // Describe how we will use the texture so that the GPU may handle it efficiently.
-        let usage = wgpu::TextureUsage::SAMPLED;
-        let iter = images.iter().map(|&(_, ref img)| img);
-        wgpu::Texture::load_array_from_image_buffers(device, queue, usage, iter)
-            .expect("tied to load texture array with an empty image buffer sequence")
-    };
-    let layer = 0;
-    let texture_view = texture_array.view().layer(layer).build();
+    Model {this_capture_frame, last_capture_frame, noise, points, incs,xOff, yOff, new_frame, last_calc, inc}
+} 
 
-    // Create the sampler for sampling from the source texture.
-    let sampler = wgpu::SamplerBuilder::new().build(device);
+fn update(app: &App, m: &mut Model, _update: Update) {
 
-    let bind_group_layout = create_bind_group_layout(device, texture_view.component_type());
-    let bind_group = create_bind_group(device, &bind_group_layout, &texture_view, &sampler);
-    let pipeline_layout = create_pipeline_layout(device, &bind_group_layout);
-    let render_pipeline = create_render_pipeline(
-        device,
-        &pipeline_layout,
-        &vs_mod,
-        &fs_mod,
-        format,
-        msaa_samples,
-    );
+    // ref:
+    //https://doc.rust-lang.org/nightly/core/time/struct.Duration.html
+    //let millis = Duration::from_millis(100).as_millis();
 
-    // Create the vertex buffer.
-    let vertices_bytes = vertices_as_bytes(&VERTICES[..]);
-    let usage = wgpu::BufferUsage::VERTEX;
-    let vertex_buffer = device.create_buffer_with_data(vertices_bytes, usage);
+    let since_last_calc = _update.since_start.as_millis() - m.last_calc.as_millis();
 
-    Model {
-        current_layer: 0.0,
-        texture_array,
-        texture_view,
-        sampler,
-        bind_group_layout,
-        bind_group,
-        vertex_buffer,
-        render_pipeline,
+    if since_last_calc > 10  {
+
+        m.last_calc = _update.since_start;
+
+        m.inc += 1.5;
+
+        m.new_frame = true;
+
+        for inc in m.incs.iter_mut() {
+            *inc += 0.05;
+        }
+
+        if m.this_capture_frame != m.last_capture_frame {
+            m.last_capture_frame = m. this_capture_frame;
+        }
+    
+        if CAPTURE {
+            m.this_capture_frame += 1;
+        }
+    
+        
+    } else {
+        m.new_frame = false;
     }
 }
 
-fn update(app: &App, model: &mut Model, update: Update) {
-    // Update which layer in the texture array that are viewing.
-    let window = app.main_window();
-    let device = window.swap_chain_device();
+fn view(app: &App, m: &Model, frame: Frame) {
 
-    // Determine how fast to play back the frames based on the mouse x.
-    let win_rect = window.rect();
-    let fps = map_range(
-        app.mouse.x,
-        win_rect.left(),
-        win_rect.right(),
-        -100.0,
-        100.0,
-    );
+    // get canvas to draw on
+    let draw  = app.draw();
+    let win   = app.window_rect();
+    let t     = app.time;
 
-    // Update which layer we are viewing based on the playback speed and layer count.
-    let layer_count = model.texture_array.array_layer_count();
-    model.current_layer = fmod(
-        model.current_layer + update.since_last.secs() as f32 * fps,
-        layer_count as f32,
-    );
+    draw.background().color(rgba(1.0, 0.4, 0.3, 1.0));
+    
+    for n in (0..20).rev() { 
 
-    // Update the view and the bind group ready for drawing.
-    let layer = model.current_layer as u32;
-    model.texture_view = model.texture_array.view().layer(layer).build();
-    model.bind_group = create_bind_group(
-        device,
-        &model.bind_group_layout,
-        &model.texture_view,
-        &model.sampler,
-    );
-}
+        let atten = 0.1;
+        let scale = (n as f32) * atten;
+        let mut xStore = 0.0;
+        let mut yStore = 0.0;
 
-fn view(_app: &App, model: &Model, frame: Frame) {
-    let mut encoder = frame.command_encoder();
-    let mut render_pass = wgpu::RenderPassBuilder::new()
-        .color_attachment(frame.texture_view(), |color| color)
-        .begin(&mut encoder);
-    render_pass.set_bind_group(0, &model.bind_group, &[]);
-    render_pass.set_pipeline(&model.render_pipeline);
-    render_pass.set_vertex_buffer(0, &model.vertex_buffer, 0, 0);
-    let vertex_range = 0..VERTICES.len() as u32;
-    let instance_range = 0..1;
-    render_pass.draw(vertex_range, instance_range);
-}
+        let rad_a = 20.0;
+        let rad_b = 20.0;
+        let num_cusps = 12.0;
 
-// Load a directory of images and returns them sorted by filename alongside their dimensions.
-// This function assumes all the images have the same dimensions.
-fn load_images(dir: &Path) -> (Vec<(PathBuf, RgbaImage)>, (u32, u32)) {
-    let mut images = vec![];
-    let mut dims = (0, 0);
-    for entry in std::fs::read_dir(dir).unwrap() {
-        let entry = entry.unwrap();
-        let path = entry.path();
-        let image = match image::open(&path) {
-            Ok(img) => img.into_rgba(),
-            Err(err) => {
-                eprintln!("failed to open {} as an image: {}", path.display(), err);
-                continue;
-            }
-        };
-        let (w, h) = image.dimensions();
-        dims = (w, h);
-        images.push((path, image));
+
+        let pts = (0..ANGLES + 1).rev().map(|i| {
+
+            let inc =  ( (360 / ANGLES * i) as f32).to_radians();
+            let ix  = i as usize;
+            
+            let x = ( (num_cusps-1.0) * inc.cos() * rad_b) + (( (num_cusps -1.0) * inc).cos() * rad_a );
+            //let y = (inc.sin() * rad_b;
+            let y = ( (num_cusps-1.0) * inc.sin() * rad_b) + (( (num_cusps -1.0) * inc).sin() * rad_b );
+
+            let r = 0.4;
+
+            let mut xOff = 0.0;
+            let mut yOff = 0.0;
+
+    
+            pt2(x + xOff, y + yOff)
+
+
+        });  
+        
+        let hue = app.time * 2.0 * PI;
+        
+        let color = hsla(hue, 0.5, 0.5, 1.0);
+        //let color = rgba(0.0, 0.0, 0.0, 1.0);
+        // let draw = draw.rotate( (t.sin() * n as f32) * 0.0001);
+        let draw = draw.rotate( 3.0*PI/2.0 );
+        let draw = draw.translate(pt3(-80.0, 0.0, 0.0));
+
+
+        if n % 2 == 0 {
+            draw
+            .scale(scale)
+            .polygon()
+            .color(color)
+            .no_fill()
+            .stroke(rgba(0.0, 1.0, 0.5, 1.0))
+            .stroke_weight(3.0 + (0.9 * scale))
+            .points(pts)
+            ;
+        } else {
+            draw
+            .scale(scale)
+            .polygon()
+            //.color(BLUE)
+            .no_fill()
+            .stroke(rgba(0.0, 0.5, 0.8, 0.5))
+            .stroke_weight(2.5 + (0.5 * scale))
+            .points(pts)
+            ;
+        }
+
     }
-    images.sort_by_key(|(path, _)| path.clone());
-    (images, dims)
-}
 
-fn create_bind_group_layout(
-    device: &wgpu::Device,
-    texture_component_type: wgpu::TextureComponentType,
-) -> wgpu::BindGroupLayout {
-    wgpu::BindGroupLayoutBuilder::new()
-        .sampled_texture(
-            wgpu::ShaderStage::FRAGMENT,
-            false,
-            wgpu::TextureViewDimension::D2,
-            texture_component_type,
-        )
-        .sampler(wgpu::ShaderStage::FRAGMENT)
-        .build(device)
-}
 
-fn create_bind_group(
-    device: &wgpu::Device,
-    layout: &wgpu::BindGroupLayout,
-    texture: &wgpu::TextureView,
-    sampler: &wgpu::Sampler,
-) -> wgpu::BindGroup {
-    wgpu::BindGroupBuilder::new()
-        .texture_view(texture)
-        .sampler(sampler)
-        .build(device, layout)
-}
 
-fn create_pipeline_layout(
-    device: &wgpu::Device,
-    bind_group_layout: &wgpu::BindGroupLayout,
-) -> wgpu::PipelineLayout {
-    let desc = wgpu::PipelineLayoutDescriptor {
-        bind_group_layouts: &[&bind_group_layout],
-    };
-    device.create_pipeline_layout(&desc)
-}
+    // put everything on the frame
+    draw.to_frame(app, &frame).unwrap();
 
-fn create_render_pipeline(
-    device: &wgpu::Device,
-    layout: &wgpu::PipelineLayout,
-    vs_mod: &wgpu::ShaderModule,
-    fs_mod: &wgpu::ShaderModule,
-    dst_format: wgpu::TextureFormat,
-    sample_count: u32,
-) -> wgpu::RenderPipeline {
-    wgpu::RenderPipelineBuilder::from_layout(layout, vs_mod)
-        .fragment_shader(fs_mod)
-        .color_format(dst_format)
-        .add_vertex_buffer::<Vertex>(&wgpu::vertex_attr_array![0 => Float2])
-        .sample_count(sample_count)
-        .primitive_topology(wgpu::PrimitiveTopology::TriangleStrip)
-        .build(device)
-}
+    
 
-// See the `nannou::wgpu::bytes` documentation for why this is necessary.
-fn vertices_as_bytes(data: &[Vertex]) -> &[u8] {
-    unsafe { wgpu::bytes::from_slice(data) }
+
+    // end draw frame ---------------------------------------------------------
+
+    
+    if m.this_capture_frame != m.last_capture_frame {
+            
+        // let mut owned_string: String = "hello ".to_owned();
+        // let borrowed_string: String = "output/" + app.exe_name().unwrap() + ".png";
+    
+        let directory  = "captures/".to_string();
+        let app_name   = app.exe_name().unwrap().to_string();
+        // let frame_num  = m.this_capture_frame.to_string();
+        let extension  = ".png".to_string();
+
+        let frame_num = format!("{:05}", m.this_capture_frame);
+
+        let path = format!("{}{}{}", directory, frame_num, extension);
+
+        app.main_window().capture_frame(path);
+        
+    }
 }
