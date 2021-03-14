e65fcbffe6a892f8549a37d1ba95388e43cb65c1
mmansion <mikhail@transmission.studio>
2021-03-13 20:16:17 -0500

    Basic grid impl

--- sketches/src/lib/grid.rs
+++ sketches/src/lib/grid.rs
@@ -5,28 +5,48 @@ pub struct Grid {
     pub width     : i32,
     pub height    : i32,
     pub points    : Vec <Vector2>,
-    pub ptSize : f32,
+    pub pt_size   : f32,
+
+    row_spacing : i32,
+    col_spacing : i32,
+    row_start_pts : Vec <Vector2>,
+    col_start_pts : Vec <Vector2>,
 }
 
 impl Grid {
 
-    pub fn new(rows: f32, cols: f32, rowSpacing: i32, colSpacing: i32, rect: &Rect) -> Self {
+    pub fn new(rows: f32, cols: f32, row_spacing: i32, col_spacing: i32, rect: &Rect) -> Self {
         
         let width    = rect.w() as i32;
         let height   = rect.h() as i32;
-        let position = vec2(0.0, 0.0);
-        let points   = Vec::new();
-        let ptSize   = 10.0;
+
+        let mut position  = vec2(0.0, 0.0);
+        let mut points    = Vec::new();
+        let mut row_start_pts = Vec::new();
+        let mut col_start_pts = Vec::new();
+        
+        let pt_size  = 10.0;
+
+        let row_spacing = row_spacing;
+        let col_spacing = col_spacing;
+
 
         //--------------------------------------------------------
 
-        for row in 0..( rowSpacing + 1 ) {
-            let y =  ((height / rowSpacing * row) + (-height/2)) as f32;
-    
-            for col in 0..(colSpacing + 1) {
-                let x =  ( (width/colSpacing  * col) + (-width/2) ) as f32;
+        for row in 0..( row_spacing + 1 ) {
+            let y =  ((height / row_spacing * row) + (-height/2)) as f32;
+            
+            for col in 0..(col_spacing + 1) {
+                let x =  ( (width/col_spacing  * col) + (-width/2) ) as f32;
                 
                 points.push(pt2(x, y));
+
+                if row == 0 {
+                    col_start_pts.push(pt2(x, y));
+                }
+                if col == 0 {
+                    row_start_pts.push(pt2(x,y));
+                }
             } 
         }
 
@@ -37,18 +57,60 @@ impl Grid {
             width,
             height,
             points,
-            ptSize
+            pt_size,
+            row_spacing,
+            col_spacing,
+            row_start_pts,
+            col_start_pts
         }
     }
 
-    pub fn display(&self, draw: &Draw) {
+    pub fn draw(&self, draw: &Draw) {
 
-        for i in 0..self.points.len() {
+        for p in 0..self.points.len() {
             draw.ellipse()
-            .xy(self.points[i])
-            .radius( self.ptSize )
+            .xy(self.points[p])
+            .radius( self.pt_size )
             .color(GRAY); 
         }
+
+        // draw row lines
+        for r in 0..self.row_start_pts.len() {
+   
+            let start_pt = self.row_start_pts[r];
+
+            let end_pt = pt2(
+                self.row_start_pts[r].x + self.width as f32, 
+                self.row_start_pts[r].y );
+
+            draw
+            .line()
+            .color(GRAY)
+            .points(start_pt, end_pt)
+            ;
+        }
+
+        // draw col lines
+        for c in 0..self.col_start_pts.len() {
+
+            let start_pt = self.col_start_pts[c];
+
+            let end_pt = pt2(
+                self.col_start_pts[c].x, 
+                self.col_start_pts[c].y + self.height as f32 );
+
+            draw
+            .line()
+            .color(GRAY)
+            .points(start_pt, end_pt)
+            ;
+        }
+
+     
+
+
+
+        
     }
 }
 
