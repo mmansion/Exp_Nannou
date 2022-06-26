66464564cf74c4489aca889f11efa5adb704f629
mmansion <mikhail@transmission.studio>
2022-06-25 21:10:45 -0400

    Change scratch to examples

--- sketchbook/examples/image-basics.rs
+++ sketchbook/examples/image-basics.rs
@@ -33,9 +33,7 @@ fn model(app: &App) -> Model {
     let assets = app.assets_path().unwrap();
 
     //construct the path
-    let img_path = assets
-        .join("images")
-        .join("cat.jpg");
+    let img_path = assets.join("images").join("cat.jpg");
     
     //load the texture/image
     let texture = wgpu::Texture::from_path(app, img_path).unwrap();
@@ -49,6 +47,7 @@ fn model(app: &App) -> Model {
 fn view(app: &App, m: &Model, frame: Frame) {
     let draw = app.draw();
     let win = app.window_rect();
+
     draw.background().color(WHITE);
    
     draw.texture(&m.texture);
